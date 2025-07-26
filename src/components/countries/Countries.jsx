0a68768efe477e0fac0e6aaeeaafef2ea52745c4
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-07-26 20:04:07 +0600

    use style and destructuring

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -4,6 +4,9 @@ import './Countries.css';
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
   useEffect(() => {
     fetch(
       "https://restcountries.com/v3.1/all?fields=name,flags,capital,population,region,subregion,languages,currencies,cca2"
@@ -12,16 +15,29 @@ const Countries = () => {
       .then((data) => setCountries(data));
   }, []);
 
+  const handleVisitedCountries = visitedCountry =>{
+    console.log('add countries to visited container')
+    console.log(visitedCountry);
+  }
+
   return (
     <div>
       <h2>Countries: {countries.length}</h2>
+      
+      <div>
+        <h3>Visited Countries Container</h3>
+      </div>
+
       <div className="country-container">
         {countries.map((country) => (
           <Country
             key={country.name.official}
+
             country={country.name}
             countryFlag={country.flags}
             countryCode={country.cca2}
+
+            handleVisitedCountries = {handleVisitedCountries}
           ></Country>
         ))}
       </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country, countryFlag, countryCode}) => {
-    console.log(country, countryFlag, countryCode)
+const Country = ({country, countryFlag, countryCode, handleVisitedCountries}) => {
+    // console.log(country, countryFlag, countryCode)
 
     const [visited, setVisited] = useState(false);
 
@@ -9,13 +9,17 @@ const Country = ({country, countryFlag, countryCode}) => {
         setVisited(!visited)
     }
     
+    console.log(handleVisitedCountries)
     
     return (
-        <div className='country'>
+        <div className={`country ${visited ? 'bgColorVisited' : 'bgColorNonVisited'}`}>
             
             <img src={countryFlag.png} alt="" />
-            <h2>{country?.common}</h2>
-            <p><small>Code: {countryCode}</small></p>
+            <h2 style={{color: visited ? 'black' : 'gray'}}>{country?.common}</h2>
+            <p><small style={{color: visited ? 'black' : 'gray'}}>Code: {countryCode}</small></p>
+
+            <button>Visited Container</button>
+            <hr />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? 'I have visited' : 'I want to visit'}
         </div>
