edebe245bc2a2fd79700d1b93b88c9c5b93a3529
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-07-26 19:32:59 +0600

    using usestate to toggle button

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from "react";
 import Country from "../country/Country";
+import './Countries.css';
 
 const Countries = () => {
-    
-    const [countries, setCountries] = useState([])
-    useEffect(() =>{
-        fetch('https://restcountries.com/v3.1/all?fields=name,flags,capital,population,region,subregion,languages,currencies')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flags,capital,population,region,subregion,languages,currencies,cca2"
+    )
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-    return (
-        <div>
-           <h2>Countries: {countries.length}</h2>
-           {
-            countries.map(country => <Country key={country.name.official} country={country.name} countryFlag={country.flags}></Country>)
-           }
-        </div>
-    );
+  return (
+    <div>
+      <h2>Countries: {countries.length}</h2>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country
+            key={country.name.official}
+            country={country.name}
+            countryFlag={country.flags}
+            countryCode={country.cca2}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country, countryFlag}) => {
-    console.log(country, countryFlag)
+const Country = ({country, countryFlag, countryCode}) => {
+    console.log(country, countryFlag, countryCode)
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(!visited)
+    }
     
     
     return (
         <div className='country'>
-            <p>Name: {country?.common}</p>
+            
             <img src={countryFlag.png} alt="" />
+            <h2>{country?.common}</h2>
+            <p><small>Code: {countryCode}</small></p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            {visited ? 'I have visited' : 'I want to visit'}
         </div>
     );
 };
